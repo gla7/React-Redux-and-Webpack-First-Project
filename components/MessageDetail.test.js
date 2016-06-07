161fac0a2d54f53237065edbe622ec4c6620f379
Gabriel Leon <gabriel.leonaveleyra@tapinfluence.com>
2016-06-07 10:12:11 -0600

    added more prospective tests

--- components/MessageDetail.test.js
+++ components/MessageDetail.test.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import expect from "expect";
+import { shallow } from 'enzyme';
+// import { MessageDetail } from './MessageDetail'
 
 describe ('Component: MessageDetail', () =>{
-	it ('does all the things', () => {
+	it ('passes the test of actually begin running tests', () => {
 		expect(true).toEqual(true)
-	})
+	});
+	it ('opens up the reasons why you are a good candidate when you click yes');
+	it ('opens up the reasons why you are not interested if you click no');
+	it ('shows whatever feedback you input after responding');
 })
